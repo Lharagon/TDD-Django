5ffd99e89537770b7aad8d5adf52406d92ffa501
lgar <luis.garcia@loni.usc.edu>
2018-04-02 17:57:55 -0700

    Add helper method

--- lists/tests.py
+++ lists/tests.py
@@ -16,5 +16,3 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
-
-
